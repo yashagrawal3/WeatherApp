ead47894e04f033f3b18f42ef2d1056b077a2686
Yash Agrawal <yagrawal900@gmail.com>
2017-05-31 11:24:06 +0530

    Added bootswatch styling:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
+import "bootswatch/superhero/bootstrap.css";
 
 const PLACES = [
   { name: "Palo Alto", zip: "94303" },
@@ -54,18 +57,36 @@ class App extends Component {
   render() {
     const activePlace = this.state.activePlace;
     return (
-      <div className="App">
-        {PLACES.map((place, index) => (
-          <button
-            key={index}
-            onClick={() => {
-              this.setState({ activePlace: index });
-            }}
-          >
-            {place.name}
-          </button>
-        ))}
-        <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} />
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              React Simple Weather App
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+        <Grid>
+          <Row>
+            <Col md={4} sm={4}>
+              <h3>Select a city</h3>
+              <Nav
+                bsStyle="pills"
+                stacked
+                activeKey={activePlace}
+                onSelect={index => {
+                  this.setState({ activePlace: index });
+                }}
+              >
+                {PLACES.map((place, index) => (
+                  <NavItem key={index} eventKey={index}>{place.name}</NavItem>
+                ))}
+              </Nav>
+            </Col>
+            <Col md={8} sm={8}>
+              <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
